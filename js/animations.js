4e53110122d9efbc389e788e67d9f8c570a11ac1
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-02-14 20:44:48 -0600

    Fade in some text

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,12 @@
 // NOTICE: This file is jQuery-only, and will NOT work on any pages that do not have jQuery imported
 
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log("Initialized jQuery functions");
     
-    $('.jq-loader').delay(5000).fadeOut('slow');
+     setTimeout(function() {
+        $("#jq-loader").addClass("hide");
+         setTimeout(function(){
+            $(".fadeIn").fadeIn("slow");
+         }, 200);
+     }, 5000);
 });
