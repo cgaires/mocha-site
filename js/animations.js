3d1da010d4d605e23fba345a1beec5fc3d8e60ff
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-02-14 21:02:42 -0600

    Create animations.js

---
+++ js/animations.js
@@ -0,0 +1,13 @@
+// NOTICE: This file is jQuery-only, and will NOT work on any pages that do not have jQuery imported
+
+$(document).ready(function() {
+    console.log("Initialized jQuery functions");
+    
+     setTimeout(function() {
+        $("#jq-loader").addClass("hide");
+         setTimeout(function() {
+            $(".hide").fadeIn("slow");
+            $(".fadeIn").fadeIn("slow");
+         }, 200);
+     }, 5000);
+});
