2d50a43f3bd92f7784191cd0360887d0a7c63dd0
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-02-14 20:55:45 -0600

    Import new one from development branch

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-// NOTICE: This file is jQuery-only, and will NOT work on any pages that do not have jQuery imported
-
-$(document).ready(function() {
-    console.log("Initialized jQuery functions");
-    
-     setTimeout(function() {
-        $('#jq-loader').addClass("hide");
-     }, 5000);
-});
