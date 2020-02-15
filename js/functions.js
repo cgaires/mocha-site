92d8e1166f4a298a4089a7396273a39ec1ce0460
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-02-14 22:47:23 -0600

    Create functions.js

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function httpGet(url) // Fetch current coin info with api
+ {
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
