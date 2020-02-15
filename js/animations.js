bae71b62f15f670926ece554c864da70284a0394
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-02-14 19:51:06 -0600

    Hide loader after 5 seconds

---
+++ js/animations.js
@@ -0,0 +1,7 @@
+// NOTICE: This file is jQuery-only, and will NOT work on any pages that do not have jQuery imported
+
+$( document ).ready(function() {
+    console.log("Initialized jQuery functions");
+    
+    $('.jq-loader').delay(5000).fadeOut('slow');
+});
