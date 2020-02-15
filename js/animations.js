1adf28cb1b03995fb35894995de8054a5da5904b
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-02-14 20:13:10 -0600

    Reformat

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,6 @@
 // NOTICE: This file is jQuery-only, and will NOT work on any pages that do not have jQuery imported
 
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log("Initialized jQuery functions");
     
      setTimeout(function() {
